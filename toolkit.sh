4c01342504b3ff8f9b42de0711e55245cd453330
CarlosAndreo <c.andreo@uma.es>
2024-10-17 13:57:34 +0000

    Delete python installation and add set -ex

--- toolkit.sh
+++ toolkit.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -ex
+
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 
 # Check if a token argument was provided
@@ -10,25 +14,6 @@ fi
 
 echo "Usage: $0 with token $1"
 GITHUB_TOKEN=$1
-
-# Check if Python is installed
-PYTHON_VERSION="3.12.6"
-PYTHON_BIN="/usr/local/bin/python${PYTHON_VERSION%.*}"
-
-if python3 --version &>/dev/null; then
-    echo "Python is already installed."
-else
-    echo "Python not found. Installing Python..."
-    wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
-    tar xvf Python-${PYTHON_VERSION}.tgz
-    cd Python-${PYTHON_VERSION}/
-    ./configure --enable-optimizations
-    make altinstall
-    ${PYTHON_BIN} -m ensurepip --default-pip
-    ${PYTHON_BIN} -m pip install --upgrade pip setuptools wheel
-    cd ..
-    rm -rf Python-${PYTHON_VERSION}*
-fi
 
 # Check if Git is installed
 if git --version &>/dev/null; then
