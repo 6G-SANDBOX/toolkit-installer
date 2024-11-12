dcf508fed156c29f220a6e5e2b020f593e720569
CarlosAndreo <c.andreo@uma.es>
2024-11-12 16:49:51 +0000

    Refactor: improve error messages and streamline user checks in zero_phase.py

--- src/zero_phase.py
+++ src/zero_phase.py
@@ -16,48 +16,46 @@ def check_user() -> None:
     """
     Check if the script is being run as root
     """
-    print()
     msg("info", "[USER CHECK]")
-    print()
     res = run_command("whoami")
     if res["rc"] != 0:
         msg("error", "Could not run woami command for user checking")
     if res["stdout"] != "root":
-        msg("error", "Current user: " + res["stdout"] + "Please, run this script as root. The script requires root acces in order to modify /etc/one/oned.conf configuration file.")
+        stdout = res["stdout"]
+        msg("error", f"Current user: {stdout}. Please, run this script as root. The script requires root acces in order to modify /etc/one/oned.conf configuration file.")
 
 def check_one_health() -> None:
     """
     Check the health of the OpenNebula installation
     """
-    print()
     msg("info", "[OPENNEBULA HEALTHCHECK]")
-    print()
     # Check that CLI tools are working and can be used => this implies that XMLRPC API is healthy and reachable
     command = "onevm list"
     res = run_command(command)
     if res["rc"] != 0:
-        msg("error", "OpenNebula CLI healthcheck failed. Command: " + command)
+        msg("error",  f"OpenNebula CLI healthcheck failed. Command: '{command}'")
 
     command = "onevm list"
     res = run_command(command)
     if res["rc"] != 0:
-        msg("error", "OpenNebula CLI healthcheck failed. Command: " + command)
+        msg("error",  f"OpenNebula CLI healthcheck failed. Command: '{command}'")
 
     command = "onedatastore list"
     res = run_command(command)
     if res["rc"] != 0:
-        msg("error", "OpenNebula CLI healthcheck failed. Command: " + command)
+        msg("error",  f"OpenNebula CLI healthcheck failed. Command: '{command}'")
 
     command = "oneflow list"
     res = run_command(command)
     if res["rc"] != 0:
-        msg("error", "OpenNebula CLI healthcheck failed. Command: " + command)
+        msg("error", f"OpenNebula CLI healthcheck failed. Command: '{command}'")
 
     # Testing onegate health with curl
-    command = "curl " + get_onegate_endpoint()
+    onegate_endpoint = get_onegate_endpoint()
+    command = f"curl {onegate_endpoint}"
     res = run_command(command)
     if res["rc"] != 0:
-        msg("error", "OpenNebula CLI healthcheck failed. Command: " + command)
+        msg("error", f"OpenNebula CLI healthcheck failed. Command: '{command}'")
 
     # CHANGE: pending to add futher health checks
     msg("info", "OpenNebula is healthy")
@@ -66,7 +64,7 @@ def get_onegate_endpoint() -> str:
     command = "cat /etc/one/oned.conf | grep 'ONEGATE_ENDPOINT ='" # Mejor entrar en el fichero y aplicar una expresión regular
     res = run_command(command)
     if res["rc"] != 0:
-        msg("error", "Unable to run '" + command + "'")
+        msg("error", f"Unable to run '{command}' command")
     if not res["stdout"]:
         msg("error", "ONEGATE_ENDPOINT not found in the OpenNebula configuration")
     return res["stdout"].split('"')[1]
