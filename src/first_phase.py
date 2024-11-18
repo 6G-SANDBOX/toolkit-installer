fb69443e06768a03b13ccee42b354f30d2832be7
CarlosAndreo <c.andreo@uma.es>
2024-11-18 13:51:38 +0000

    Feat: enhance user prompts in first_phase.py for better clarity and validation

--- src/first_phase.py
+++ src/first_phase.py
@@ -15,11 +15,13 @@ def _create_site(sites_path: str) -> str:
     :param sites_path: the path to the sites repository, ``str``
     :return: the name of the site, ``str``
     """
-    site = ask_text("Enter the name of the site:")
+    site = ask_text(prompt="Enter the name of the site (mandatory insert value):", default="")
     sites = git_branches(sites_path)
     while True:
         if site in sites:
-            site = ask_text("Site already exists, enter a new name:")
+            site = ask_text(prompt="Site already exists, enter a new name:", default="")
+        elif site == "":
+            site = ask_text(prompt="Site name cannot be empty, enter a new name:", default="")
         else:
             break
     return site
@@ -46,10 +48,10 @@ def _update_site_config(site_core: str) -> dict:
     add_fields = ask_confirm("Do you want to add new components?", default=False)
     if add_fields:
         while True:
-            new_key = ask_text("Enter the name of the new field (or press Enter to stop):")
+            new_key = ask_text(prompt="Enter the name of the new field (or press Enter to stop):", default="")
             if not new_key:
                 break
-            new_value = ask_text(f"Enter the value for '{new_key}':")
+            new_value = ask_text(prompt=f"Enter the value for '{new_key}':", default="")
             if new_key in updated_data:
                 msg("error", f"Field '{new_key}' already exists")
             updated_data[new_key] = new_value
@@ -62,27 +64,27 @@ def first_phase() -> None:
     github_sites_https = get_env_var("GITHUB_SITES_HTTPS")
     sites_path = save_temp_directory("6G-Sandbox-Sites")
     git_clone(github_sites_https, sites_path)
-    msg("Repository 6G-Sandbox-Sites cloned successfully")
+    msg("info", "Repository 6G-Sandbox-Sites cloned successfully")
     site = _create_site(sites_path)
-    msg(f"Site name: '{site}'")
+    msg("info", f"Site name: '{site}'")
     git_switch(sites_path, site)
-    msg(f"Site branch '{site}' created successfully in local")
+    msg("info", f"Site branch '{site}' created successfully in local")
     site_path = save_temp_directory(site)
-    msg(f"Site directory '{site_path}' created successfully")
+    msg("info", f"Site directory '{site_path}' created successfully")
     dummy_core_path = temp_path(os.path.join("6G-Sandbox-Sites", ".dummy_site", "core.yaml"))
     site_core_path = os.path.join(site_path, "core.yaml")
     run_command(f"cp {dummy_core_path} {site_core_path}")
-    msg(f"Site structure copied successfully from '{dummy_core_path}' to '{site_core_path}'")
+    msg("info", f"Site structure copied successfully from '{dummy_core_path}' to '{site_core_path}'")
     site_core = load_yaml(site_core_path)
     site_config = _update_site_config(site_core)
     save_yaml(site_core_path, site_config)
-    msg(f"Site configuration updated successfully")
+    msg("info", f"Site configuration updated successfully")
     sites_token = ask_text("Enter the token for the site:")
-    msg(f"Token '{sites_token}' generated successfully")
+    msg("info", f"Token '{sites_token}' generated successfully")
     token_path = save_temp_file(data=sites_token, file_path="sites_token.txt", mode="w", encoding="utf-8")
-    msg(f"Token saved successfully in '{token_path}'")
+    msg("info", f"Token saved successfully in '{token_path}'")
     run_command(f"ansible-vault encrypt {site_core_path} --vault-password-file {token_path}")
-    msg(f"File '{site_core_path}' encrypted successfully")
+    msg("info", f"File '{site_core_path}' encrypted successfully")
     git_add(sites_path, ".")
     git_commit(sites_path, f"Add site '{site}'")
     git_push(sites_path, site)
