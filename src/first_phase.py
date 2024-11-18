15fd8c9ff92bb1440c590f274bccd82dd14facfb
CarlosAndreo <c.andreo@uma.es>
2024-11-18 09:32:48 +0000

    Refactor: update marketplace ID handling to use string type and improve function signatures

--- src/first_phase.py
+++ src/first_phase.py
@@ -4,16 +4,17 @@ from time import sleep
 from src.zero_phase import check_one_health
 from src.utils.dotenv import get_env_var
 from src.utils.cli import run_command
-from src.utils.file import load_file, loads_json, save_file, save_temp_file
+from src.utils.file import load_file, loads_json, save_file
 from src.utils.logs import msg
+from src.utils.temp import save_temp_file
 
-def _find_marketplace_id(marketplace_name: str, marketplace_endpoint: str) -> int:
+def _find_marketplace_id(marketplace_name: str, marketplace_endpoint: str) -> str:
     """
     Check if the 6G-SANDBOX marketplace is already present in the OpenNebula installation
     
     :param marketplace_name: the name of the marketplace, ``str``
     :param marketplace_endpoint: the endpoint of the marketplace, ``str``
-    :return: the ID of the marketplace if it is present, ``int``
+    :return: the ID of the marketplace if it is present, ``str``
     """
     msg("info", "[6G-SANDBOX MARKETPLACE CHECK]")
     res = run_command("onemarket list -j")
@@ -24,17 +25,17 @@ def _find_marketplace_id(marketplace_name: str, marketplace_endpoint: str) -> in
     for marketplace in marketplaces["MARKETPLACE_POOL"]["MARKETPLACE"]:
         if marketplace["NAME"] == marketplace_name and marketplace["TEMPLATE"]["ENDPOINT"] == marketplace_endpoint:
             msg("info", "6G-SANDBOX marketplace already present with ID " + marketplace["ID"])
-            marketplace_id = int(marketplace["ID"])
+            marketplace_id = marketplace["ID"]
             break
     return marketplace_id
 
-def _add_sandbox_marketplace(marketplace_name: str, marketplace_endpoint: str) -> int:
+def _add_sandbox_marketplace(marketplace_name: str, marketplace_endpoint: str) -> str:
     """
     Add the 6G-SANDBOX marketplace to the OpenNebula installation
     
     :param marketplace_name: the name of the marketplace, ``str``
     :param marketplace_endpoint: the endpoint of the marketplace, ``str``
-    :return: the ID of the marketplace, ``int``
+    :return: the ID of the marketplace, ``str``
     """
     marketplace_content = dedent(f"""
         NAME = {marketplace_name}
@@ -48,13 +49,13 @@ def _add_sandbox_marketplace(marketplace_name: str, marketplace_endpoint: str) -
         msg("error", "The 6G-SANDBOX marketplace could not be registered. Please, review the marketplace_template file")
     marketplace_id = res["stdout"].split()[1]
     msg("info", f"6G-SANDBOX marketplace registered successfully with ID {marketplace_id}")
-    return int(marketplace_id)
+    return marketplace_id
 
-def _is_marketplace_ready(marketplace_id: int) -> bool:
+def _is_marketplace_ready(marketplace_id: str) -> bool:
     """
     Check if the 6G-SANDBOX marketplace is ready to be used
     
-    :param marketplace_id: the ID of the marketplace, ``int``
+    :param marketplace_id: the ID of the marketplace, ``str``
     :return: whether the marketplace is ready, ``bool``
     """
     res = run_command(f"onemarket show -j {marketplace_id}")
@@ -74,7 +75,7 @@ def _set_marketplace_monitoring_interval(interval: int) -> int:
     lines = oned_conf.splitlines(keepends=True)
     for i, line in enumerate(lines):
         if line.startswith("MONITORING_INTERVAL_MARKET"):
-            old_interval = int(line.split("=")[1].strip())
+            old_interval = line.split("=")[1].strip()
             lines[i] = f"MONITORING_INTERVAL_MARKET = {interval}\n"
             break
     if old_interval is not None:
@@ -93,6 +94,9 @@ def _restart_oned() -> None:
     msg("info", "OpenNebula daemon restarted")
 
 def first_phase() -> None:
+    """
+    Run the first phase of the 6G-SANDBOX deployment
+    """
     marketplace_name = get_env_var("OPENNEBULA_MARKETPLACE_NAME")
     marketplace_endpoint = get_env_var("OPENNEBULA_MARKETPLACE_ENDPOINT")
     marketplace_id = _find_marketplace_id(marketplace_name, marketplace_endpoint)
