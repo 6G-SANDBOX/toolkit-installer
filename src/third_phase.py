bb362b5ad1891d6714e6474a6c1469233c3ca9bb
CarlosAndreo <c.andreo@uma.es>
2024-11-22 21:23:18 +0000

    Fix: update local image retrieval to local template and adjust sleep duration for image readiness

--- src/third_phase.py
+++ src/third_phase.py
@@ -3,7 +3,7 @@ from time import sleep
 from src.utils.dotenv import get_env_var
 from src.utils.logs import msg
 from src.utils.interactive import ask_select, ask_checkbox
-from src.utils.one import get_onemarket, get_user, get_group, add_marketplace, get_marketplace_monitoring_interval, update_marketplace_monitoring_interval, restart_one, check_one_health, get_appliances_marketplace, get_local_image, get_onedatastores, get_onedatastore, export_image, get_state_image, chown_image, chown_template
+from src.utils.one import get_onemarket, get_user, get_group, add_marketplace, get_marketplace_monitoring_interval, update_marketplace_monitoring_interval, restart_one, check_one_health, get_appliances_marketplace, get_local_template, get_onedatastores, get_onedatastore, export_image, get_state_image, chown_image, chown_template
 
 def _add_appliances_from_marketplace(sixg_sandbox_group: str, jenkins_user: str, marketplace_id: int, appliances: list) -> None:
     """
@@ -15,14 +15,14 @@ def _add_appliances_from_marketplace(sixg_sandbox_group: str, jenkins_user: str,
     :param appliances: the list of appliances to add, ``list``
     """
     for appliance_name in appliances:
-        if get_local_image(appliance_name) is None:
+        if get_local_template(appliance_name) is None:
             msg("info", f"Appliance {appliance_name} not present, exporting...")
             onedatastores = get_onedatastores()
             datastore = ask_select("Select the datastore where you want to store the image", onedatastores)
             datastore_id = get_onedatastore(datastore)["DATASTORE"]["ID"]
             image_id, template_id = export_image(marketplace_id=marketplace_id, appliance_name=appliance_name, datastore_id=datastore_id)
-            sleep(5)
-            while get_state_image(appliance_name) != "1":
+            sleep(10)
+            while get_state_image(image_id) != "1":
                 msg("info", "Please, wait 10s for the image to be ready...")
                 sleep(10)
             jenkins_user_id = int(get_user(username=jenkins_user)["USER"]["ID"])
