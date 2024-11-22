bdd8640107ba4419b058b269da36195391ced271
CarlosAndreo <c.andreo@uma.es>
2024-11-22 17:32:16 +0000

    Refactor: enhance user and group creation with existence checks and informative messages

--- src/second_phase.py
+++ src/second_phase.py
@@ -7,9 +7,15 @@ def second_phase() -> None:
     msg("info", "SECOND PHASE")
     default_sandbox_group = get_env_var("OPENNEBULA_SANDBOX_GROUP")
     sixg_sandbox_group = ask_text("Enter the name for the 6G-SANDBOX group:", default=default_sandbox_group, validate=True)
+    sixg_sandbox_group_data = get_group(group_name=sixg_sandbox_group)
+    if sixg_sandbox_group_data is not None:
+        sixg_sandbox_group = msg("info", "Group already exists")
     sixg_sandbox_group_id = create_group(group_name=sixg_sandbox_group)
     default_jenkins_user = get_env_var("OPENNEBULA_JENKINS_USER")
     jenkins_user = ask_text("Enter the username for the Jenkins user:", default=default_jenkins_user, validate=True)
+    jenkins_user_data = get_user(username=jenkins_user)
+    if jenkins_user_data is not None:
+        jenkins_user = msg("info", "User already exists")
     jenkins_password = ask_password("Enter the password for the Jenkins user:", validate=True)
     jenkins_user_id = create_user(username=jenkins_user, password=jenkins_password)
     assign_user_group(user_id=jenkins_user_id, group_id=sixg_sandbox_group_id)
