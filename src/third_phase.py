4657ab869127a7b63dc693977f2835885d90ab0f
CarlosAndreo <c.andreo@uma.es>
2024-11-22 18:21:32 +0000

    Refactor: rename variables for clarity in third_phase function

--- src/third_phase.py
+++ src/third_phase.py
@@ -31,13 +31,13 @@ def _add_appliances_from_marketplace(markeplace_id: int, appliances: list, sixg_
 
 def third_phase(sixg_sandbox_group: str, jenkins_user: str) -> None:
     msg("info", "THIRD PHASE")
-    marketplace_name = get_env_var("OPENNEBULA_SANDBOX_MARKETPLACE_NAME")
-    marketplace_description = get_env_var("OPENNEBULA_SANDBOX_MARKETPLACE_DESCRIPTION")
-    marketplace_endpoint = get_env_var("OPENNEBULA_SANDBOX_MARKETPLACE_ENDPOINT")
-    marketplace = get_onemarket(marketplace_name=marketplace_name)
-    if marketplace is not None:
-        msg("info", f"Marketplace {marketplace_name} not present, adding...")
-        _ = add_marketplace(marketplace_name, marketplace_description, marketplace_endpoint)
+    sixg_sandbox_marketplace_name = get_env_var("OPENNEBULA_SANDBOX_MARKETPLACE_NAME")
+    sixg_sandbox_marketplace_description = get_env_var("OPENNEBULA_SANDBOX_MARKETPLACE_DESCRIPTION")
+    sixg_sandbox_marketplace_endpoint = get_env_var("OPENNEBULA_SANDBOX_MARKETPLACE_ENDPOINT")
+    sixg_sandbox_marketplace = get_onemarket(marketplace_name=sixg_sandbox_marketplace_name)
+    if sixg_sandbox_marketplace is not None:
+        msg("info", f"Marketplace {sixg_sandbox_marketplace_name} not present, adding...")
+        sixg_sandbox_marketplace_id = add_marketplace(sixg_sandbox_marketplace_name, sixg_sandbox_marketplace_description, sixg_sandbox_marketplace_endpoint)
         force_fast_marketplace_monitoring = get_env_var("FORCE_FAST_MARKETPLACE_MONITORING")
         if force_fast_marketplace_monitoring == "false":
             msg("info", "Please, wait 600s for the marketplace to be ready...")
@@ -54,12 +54,11 @@ def third_phase(sixg_sandbox_group: str, jenkins_user: str) -> None:
             restart_one()
             check_one_health()
     else:
-        msg("info", f"Marketplace {marketplace_name} already present")
+        msg("info", f"Marketplace {sixg_sandbox_marketplace_name} already present")
+        sixg_sandbox_marketplace_id = sixg_sandbox_marketplace["MARKETPLACE"]["ID"]
 
     opennebula_public_marketplace = get_env_var("OPENNEBULA_PUBLIC_MARKETPLACE")
-    sixg_sandbox_marketplace = get_env_var("OPENNEBULA_SANDBOX_MARKETPLACE_NAME")
     opennebula_public_marketplace_id = get_onemarket(marketplace_name=opennebula_public_marketplace)["MARKETPLACE"]["ID"]
-    sixg_sandbox_marketplace_id = get_onemarket(marketplace_name=sixg_sandbox_marketplace)["MARKETPLACE"]["ID"]
     opennebula_public_marketplace_appliances = get_appliances_marketplace(marketplace_id=opennebula_public_marketplace_id)
     opennebula_public_appliances_selected = ask_checkbox("Select the appliances you want to import from the OpenNebula Public Marketplace", opennebula_public_marketplace_appliances)
     _add_appliances_from_marketplace(marketplace_id=opennebula_public_marketplace_id, appliances=opennebula_public_appliances_selected, sixg_sandbox_group=sixg_sandbox_group, jenkins_user=jenkins_user)
