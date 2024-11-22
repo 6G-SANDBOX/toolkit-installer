ea2dbabf51b5584c66d9fc0a14598cb36e02856d
CarlosAndreo <c.andreo@uma.es>
2024-11-22 14:55:29 +0000

    Refactor: enhance third phase logic to validate group and user existence before creation

--- src/third_phase.py
+++ src/third_phase.py
@@ -1,7 +1,7 @@
 from src.utils.dotenv import get_env_var
 from src.utils.interactive import ask_text, ask_checkbox, ask_password
 from src.utils.logs import msg
-from src.utils.one import get_onemarkets, create_group, create_user, assign_user_group
+from src.utils.one import get_onemarkets, get_group, create_group, get_user, create_user, assign_user_group
 
 def _select_appliances(marketplace_name: str) -> None:
     """
@@ -26,11 +26,23 @@ def third_phase() -> None:
     The third phase of the 6G-SANDBOX deployment
     """
     msg("info", "THIRD PHASE")
-    default_sixg_sandbox_group = get_env_var("OPENNEBULA_SANDBOX_GROUP")
-    sixg_sandbox_group = ask_text("Enter the name for the 6G-SANDBOX group:", default=default_sixg_sandbox_group, validate=True)
+    default_sandbox_group = get_env_var("OPENNEBULA_SANDBOX_GROUP")
+    sixg_sandbox_group = ask_text("Enter the name for the 6G-SANDBOX group:", default=default_sandbox_group, validate=True)
+    while True:
+        sixg_sandbox_group_data = get_group(group_name=sixg_sandbox_group)
+        if sixg_sandbox_group_data:
+            sixg_sandbox_group = ask_text("Group already exists. Enter new name for the 6G-SANDBOX group:", default=default_sandbox_group, validate=True)
+        else:
+            break
     sixg_sandbox_group_id = create_group(group_name=sixg_sandbox_group)
     default_jenkins_user = get_env_var("OPENNEBULA_JENKINS_USER")
     jenkins_user = ask_text("Enter the username for the Jenkins user:", default=default_jenkins_user, validate=True)
+    while True:
+        jenkins_user_data = get_user(username=jenkins_user)
+        if jenkins_user_data:
+            jenkins_user = ask_text("User already exists. Enter new username for the Jenkins user:", default=default_jenkins_user, validate=True)
+        else:
+            break
     jenkins_password = ask_password("Enter the password for the Jenkins user:", default="", validate=True)
     jenkins_user_id = create_user(username=jenkins_user, password=jenkins_password)
     assign_user_group(user_id=jenkins_user_id, group_id=sixg_sandbox_group_id)
