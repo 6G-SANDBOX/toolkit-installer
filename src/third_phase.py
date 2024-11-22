f994e35a33d94fcd760bb0315e9a4e99cd06dc6c
CarlosAndreo <c.andreo@uma.es>
2024-11-22 19:33:01 +0000

    Fix: param in add appliances function

--- src/third_phase.py
+++ src/third_phase.py
@@ -5,14 +5,14 @@ from src.utils.logs import msg
 from src.utils.interactive import ask_select, ask_checkbox
 from src.utils.one import get_onemarket, get_user, get_group, add_marketplace, get_marketplace_monitoring_interval, update_marketplace_monitoring_interval, restart_one, check_one_health, get_appliances_marketplace, get_local_image, get_onedatastores, get_onedatastore, export_image, get_state_image, chown_image, chown_template
 
-def _add_appliances_from_marketplace(markeplace_id: int, appliances: list, sixg_sandbox_group: str, jenkins_user: str) -> None:
+def _add_appliances_from_marketplace(sixg_sandbox_group: str, jenkins_user: str, marketplace_id: int, appliances: list) -> None:
     """
     Add appliances from a marketplace to the local OpenNebula
 
-    :param markeplace_id: the ID of the marketplace, ``int``
-    :param appliances: the list of appliances to add, ``list``
     :param sixg_sandbox_group: the name of the 6G-SANDBOX group, ``str``
     :param jenkins_user: the name of the Jenkins user, ``str``
+    :param marketplace_id: the ID of the marketplace, ``int``
+    :param appliances: the list of appliances to add, ``list``
     """
     for appliance_name in appliances:
         if get_local_image(appliance_name) is not None:
@@ -20,7 +20,7 @@ def _add_appliances_from_marketplace(markeplace_id: int, appliances: list, sixg_
             onedatastores = get_onedatastores()
             datastore = ask_select("Select the datastore where you want to store the image", onedatastores)
             datastore_id = get_onedatastore(datastore)["DATASTORE"]["ID"]
-            image_id, template_id = export_image(marketplace_id=markeplace_id, appliance_name=appliance_name, datastore_id=datastore_id)
+            image_id, template_id = export_image(marketplace_id=marketplace_id, appliance_name=appliance_name, datastore_id=datastore_id)
             while get_state_image(appliance_name) != "1":
                 msg("info", "Please, wait 10s for the image to be ready...")
                 sleep(10)
@@ -62,8 +62,8 @@ def third_phase(sixg_sandbox_group: str, jenkins_user: str) -> None:
     opennebula_public_marketplace_appliances = get_appliances_marketplace(marketplace_id=opennebula_public_marketplace_id)
     opennebula_public_appliances_selected = ask_checkbox("Select the appliances you want to import from the OpenNebula Public Marketplace", opennebula_public_marketplace_appliances)
     if opennebula_public_appliances_selected:
-        _add_appliances_from_marketplace(marketplace_id=opennebula_public_marketplace_id, appliances=opennebula_public_appliances_selected, sixg_sandbox_group=sixg_sandbox_group, jenkins_user=jenkins_user)
+        _add_appliances_from_marketplace(sixg_sandbox_group=sixg_sandbox_group, jenkins_user=jenkins_user, marketplace_id=opennebula_public_marketplace_id, appliances=opennebula_public_appliances_selected)
     sixg_sandbox_marketplace_appliances = get_appliances_marketplace(marketplace_id=sixg_sandbox_marketplace_id)
     sixg_sandbox_appliances_selected = ask_checkbox("Select the appliances you want to import from the 6G-SANDBOX Marketplace", sixg_sandbox_marketplace_appliances)
     if sixg_sandbox_appliances_selected:
-        _add_appliances_from_marketplace(markeplace_id=sixg_sandbox_marketplace_id, appliances=sixg_sandbox_appliances_selected, sixg_sandbox_group=sixg_sandbox_group, jenkins_user=jenkins_user)
+        _add_appliances_from_marketplace(sixg_sandbox_group=sixg_sandbox_group, jenkins_user=jenkins_user, marketplace_id=sixg_sandbox_marketplace_id, appliances=sixg_sandbox_appliances_selected)
