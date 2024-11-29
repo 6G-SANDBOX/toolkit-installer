c96f17004146299c85ca95efd2c269cc09704479
CarlosAndreo <c.andreo@uma.es>
2024-11-29 07:43:27 +0000

    Add: ansible encrypt and descrypt files

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -1,5 +1,8 @@
 import base64
 
+from ansible.parsing.vault import VaultLib, VaultSecret
+from ansible.constants import DEFAULT_VAULT_ID_MATCH
+
 def encode_base64(data: str) -> str:
     """
     Encode a string to Base64
@@ -17,3 +20,25 @@ def decode_base64(encoded_data: str) -> str:
     :return: the decoded data as bytes, ``str``
     """
     return base64.b64decode(encoded_data).decode("utf-8")
+
+def ansible_encrypt(data: str, token_path: str) -> None:
+    """
+    Encrypt a file using Ansible Vault
+
+    :param encrypted_data: the data to be encrypted, ``str``
+    :param token_path: the path to the token file, ``str``
+    """
+    secret = token_path.encode("utf-8")
+    vault = VaultLib([(DEFAULT_VAULT_ID_MATCH, VaultSecret(secret))])
+    return vault.encrypt(data)
+
+def ansible_decrypt(encrypted_data: str, token_path: str) -> None:
+    """
+    Decrypt a file using Ansible Vault
+
+    :param encrypted_data: the data to be decrypted, ``str``
+    :param token_path: the path to the token file, ``str``
+    """
+    secret = token_path.encode("utf-8")
+    vault = VaultLib([(DEFAULT_VAULT_ID_MATCH, VaultSecret(secret))])
+    return vault.decrypt(encrypted_data)
