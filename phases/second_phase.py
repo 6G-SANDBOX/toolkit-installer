737ccf2947af9ebed698d5c3aae165205243fa36
CarlosAndreo <c.andreo@uma.es>
2024-12-03 09:06:07 +0000

    Fix: remove unused import and adjust default value handling in network generation

--- phases/second_phase.py
+++ phases/second_phase.py
@@ -4,7 +4,7 @@ from phases.utils.file import get_env_var
 from phases.utils.interactive import ask_text, ask_password, ask_confirm, ask_select
 from phases.utils.logs import msg
 from phases.utils.one import add_appliances_from_marketplace, get_onemarket, add_marketplace, get_marketplace_monitoring_interval, update_marketplace_monitoring_interval, restart_one, check_one_health, get_vm, get_onegate_endpoint, add_ssh_key, get_oneflow_template_networks, get_oneflow_template_custom_attrs, instantiate_oneflow_template, get_vnets_names, get_vnet_id, chown_oneflow, get_oneflow_roles, chown_vm
-from phases.utils.temp import load_temp_file, save_temp_json_file, temp_path, save_temp_file
+from phases.utils.temp import load_temp_file, save_temp_json_file, temp_path
 
 def _parse_custom_attr(attr_string: str) -> dict:
     """
@@ -87,6 +87,7 @@ def _generate_networks_values(networks: dict) -> dict:
         _ = parser_custom_attr["input_type"]
         description = parser_custom_attr["description"]
         default_value = parser_custom_attr["default_value"]
+        default_value = default_value[:-1]
         vnets = get_vnets_names()
         vnet = ask_select(prompt=description, choices=vnets)
         vnet_id = get_vnet_id(vnet_name=vnet)
