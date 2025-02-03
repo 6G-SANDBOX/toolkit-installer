5ad8e8d7fb38de3b82b3745d16a1ce783426c465
CarlosAndreo <c.andreo@uma.es>
2025-02-03 10:40:29 +0000

    add: keyboard interrupt exception

--- phases/utils/interactive.py
+++ phases/utils/interactive.py
@@ -1,4 +1,5 @@
 import questionary
+import sys
 
 from typing import Callable, Union, List, Optional
 
@@ -11,24 +12,25 @@ def ask_text(prompt: str, default: Optional[str] = None, validate: Union[bool, C
     :param validate: if True, input is required; if False, no validation; if a function is provided, it's used for custom validation, ``Union[bool, Callable[[str], bool]]``
     :return: user input, ``str``
     """
-    if isinstance(validate, bool):
-        if validate:
+    try:
+        if isinstance(validate, bool):
+            if validate:
+                return questionary.text(
+                    prompt,
+                    default=default,
+                    validate=lambda text: bool(text.strip()) or "This field is required"
+                ).ask()
+            else:
+                return questionary.text(prompt, default=default).ask()
+        if isinstance(validate, Callable):
             return questionary.text(
                 prompt,
                 default=default,
-                validate=lambda text: bool(text.strip()) or "This field is required"
+                validate=validate
             ).ask()
-        else:
-            return questionary.text(prompt, default=default).ask()
-    elif isinstance(validate, Callable):
-        return questionary.text(
-            prompt,
-            default=default,
-            validate=validate
-        ).ask()
-    else:
-        raise ValueError("validate must be a boolean or a callable")
-    
+    except KeyboardInterrupt:
+        print("\nOperation interrupted by user")
+        sys.exit(255)
 
 def ask_select(prompt: str, choices: List[str]) -> str:
     """
@@ -38,7 +40,11 @@ def ask_select(prompt: str, choices: List[str]) -> str:
     :param choices: list of options to choose from, ``List[str]``
     :return: selected option, ``str``
     """
-    return questionary.select(prompt, choices=choices).ask()
+    try:
+        return questionary.select(prompt, choices=choices).ask()
+    except KeyboardInterrupt:
+        print("\nOperation interrupted by user")
+        sys.exit(255)
 
 def ask_checkbox(prompt: str, choices: List[str]) -> List[str]:
     """
@@ -48,7 +54,11 @@ def ask_checkbox(prompt: str, choices: List[str]) -> List[str]:
     :param choices: list of options to choose from, ``List[str]``
     :return: list of selected options, ``List[str]``
     """
-    return questionary.checkbox(prompt, choices=choices).ask()
+    try:
+        return questionary.checkbox(prompt, choices=choices).ask()
+    except KeyboardInterrupt:
+        print("\nOperation interrupted by user")
+        sys.exit(255)
 
 def ask_confirm(prompt: str, default: bool = True) -> bool:
     """
@@ -58,7 +68,11 @@ def ask_confirm(prompt: str, default: bool = True) -> bool:
     :param default: default response if the user presses Enter, ``bool``
     :return: user's confirmation, ``bool``
     """
-    return questionary.confirm(prompt, default=default).ask()
+    try:
+        return questionary.confirm(prompt, default=default).ask()
+    except KeyboardInterrupt:
+        print("\nOperation interrupted by user")
+        sys.exit(255)
 
 def ask_password(prompt: str, validate: Union[bool, Callable[[str], bool]] = False) -> str:
     """
@@ -68,18 +82,20 @@ def ask_password(prompt: str, validate: Union[bool, Callable[[str], bool]] = Fal
     :param validate: if True, input is required, ``bool``
     :return: user input, ``str``
     """
-    if isinstance(validate, bool):
-        if validate:
+    try:
+        if isinstance(validate, bool):
+            if validate:
+                return questionary.password(
+                    prompt,
+                    validate=lambda text: bool(text.strip()) or "This field is required"
+                ).ask()
+            else:
+                return questionary.password(prompt).ask()
+        if isinstance(validate, Callable):
             return questionary.password(
                 prompt,
-                validate=lambda text: bool(text.strip()) or "This field is required"
+                validate=validate
             ).ask()
-        else:
-            return questionary.password(prompt).ask()
-    elif isinstance(validate, Callable):
-        return questionary.password(
-            prompt,
-            validate=validate
-        ).ask()
-    else:
-        raise ValueError("validate must be a boolean or a callable")
+    except KeyboardInterrupt:
+        print("\nOperation interrupted by user")
+        sys.exit(255)
