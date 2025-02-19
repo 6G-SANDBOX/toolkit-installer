2a2093e556d59001622bb1eb41624337a1cc8ff9
CarlosAndreo <c.andreo@uma.es>
2025-02-19 10:15:27 +0000

    fix: some comments

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,22 +15,22 @@ echo "========== Starting Pre-Checks for Script Execution =========="
 
 echo "Checking if the script is being executed as root..."
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root. Please use 'sudo' or switch to the root user."
+    echo "This script must be run as root. Please use 'sudo' or switch to the root user"
     exit 1
 else
-    echo "Script is running as root."
+    echo "Script is running as root"
 fi
 
 echo "Detecting Ubuntu version..."
 echo "Detected Ubuntu version: ${UBUNTU_VERSION}"
 if [[ "${UBUNTU_VERSION}" != "22.04" && "${UBUNTU_VERSION}" != "24.04" ]]; then
-    echo "Unsupported Ubuntu version: ${UBUNTU_VERSION}. This script only supports Ubuntu 22.04 LTS and 24.04 LTS."
+    echo "Unsupported Ubuntu version: ${UBUNTU_VERSION}. This script only supports Ubuntu 22.04 LTS and 24.04 LTS"
     exit 1
 else
-    echo "Ubuntu version ${UBUNTU_VERSION} is supported."
+    echo "Ubuntu version ${UBUNTU_VERSION} is supported"
 fi
 
-echo "Running as root. Ubuntu version detected: ${UBUNTU_VERSION}."
+echo "Running as root. Ubuntu version detected: ${UBUNTU_VERSION}"
 
 echo "========== Pre-Checks Completed Successfully =========="
 
@@ -41,7 +41,7 @@ apt-get update
 
 echo "--------------- Installing Git ---------------"
 if git --version &>/dev/null; then
-    echo "Git is already installed."
+    echo "Git is already installed"
 else
     echo "Installing Git..."
     apt-get install -y git
@@ -49,7 +49,7 @@ fi
 
 echo "--------------- Installing Python ---------------"
 if python3 --version | awk '{print $2}' | grep -qE '^3\.1[3-9]|^[4-9]'; then
-    echo "Python ${PYTHON_VERSION} is already installed."
+    echo "Python ${PYTHON_VERSION} is already installed"
 else
     echo "Adding deadsnakes PPA and installing Python ${PYTHON_VERSION}..."
     add-apt-repository ppa:deadsnakes/ppa -y
@@ -64,7 +64,7 @@ curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR=${UV_PATH} sh
 
 echo "--------------- Cloning toolkit installer repository ---------------"
 if [[ -d ${TOOLKIT_INSTALLER_FOLDER} ]]; then
-    echo "Toolkit installer repository already cloned."
+    echo "Toolkit installer repository already cloned"
 else
     echo "Cloning toolkit installer repository..."
     git clone https://github.com/6G-SANDBOX/toolkit-installer.git ${TOOLKIT_INSTALLER_FOLDER}
